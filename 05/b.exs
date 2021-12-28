5282eb487b4ce44f005568495f542966daa2d349
Jeyko Caicedo <jeyko_caicedo@gap.com>
2021-12-28 18:11:50 +0000

    Day 5

---
+++ 05/b.exs
@@ -0,0 +1,20 @@
+{:ok, text} = File.read("input.txt")
+
+lines = text
+  |> String.split("\n")
+  |> Enum.filter(&(&1 != ""))
+  |> Enum.map(fn x -> Regex.scan(~r/\d+/, x)
+    |> List.flatten
+    |> Enum.map(&Integer.parse/1)
+    |> Enum.map(fn {n, _} -> n end)
+  end)
+
+result = lines |> Enum.reduce(Map.new(), fn [x1, y1, x2, y2], map ->
+  cond do
+    x1 == x2 -> y1..y2 |> Enum.reduce(map, fn y, map -> Map.update(map, {x1, y}, 1, &(&1 + 1)) end)
+    y1 == y2 -> x1..x2 |> Enum.reduce(map, fn x, map -> Map.update(map, {x, y1}, 1, &(&1 + 1)) end)
+    true -> Enum.zip(x1..x2, y1..y2) |> Enum.reduce(map, fn {x, y}, map -> Map.update(map, {x, y}, 1, &(&1 + 1)) end)
+  end
+end)
+
+IO.inspect(result |> Enum.filter(fn {_, v} -> v > 1 end) |> Enum.count)
